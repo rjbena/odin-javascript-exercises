f0809ce153f3ea8fbc2315a331c4ed4bad0ce2be
Ricardo J Benavides <rickyjbena@yahoo.com>
2020-08-12 12:24:13 -0700

    finished removeFromArray challenge

--- javascript-exercises/javascript-exercises-master/removeFromArray/removeFromArray.js
+++ javascript-exercises/javascript-exercises-master/removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...itemToBeDeleted) {
+    let newArr = arr;
+    for (let i = 0; i < itemToBeDeleted.length; i++) {
+     newArr = newArr.filter(a => a !== itemToBeDeleted[i]);
+  }
+  return newArr;
+};
 
-}
-
-module.exports = removeFromArray
+module.exports = removeFromArray;
