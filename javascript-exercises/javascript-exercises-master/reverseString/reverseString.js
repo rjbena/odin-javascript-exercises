090a6dc3fb1880939e78fe221e3927aed08e7d61
Ricardo J Benavides <rickyjbena@yahoo.com>
2020-08-12 11:53:01 -0700

    Completed reverse String

--- javascript-exercises/javascript-exercises-master/reverseString/reverseString.js
+++ javascript-exercises/javascript-exercises-master/reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    if (typeof string !== "string") {
+        return "ERROR";
+    }
+    let length = string .length;
+    if (length <= 0) {
+        return "";
+    }
+    let reverse = '';
+    for (let i = length-1; i >=0; i--) {
+        reverse += string.charAt(i);
+    }
+    return reverse;
 }
 
 module.exports = reverseString
