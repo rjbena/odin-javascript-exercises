06d10e55e8d59200ddbc855efa1471a8b9983009
Ricardo J Benavides <rickyjbena@yahoo.com>
2020-08-11 18:27:15 -0700

    finished repeat string

--- javascript-exercises/javascript-exercises-master/repeatString/repeatString.js
+++ javascript-exercises/javascript-exercises-master/repeatString/repeatString.js
@@ -1,4 +1,19 @@
-const repeatString = function() {
+const repeatString = function(string, repeats) {
+    if (repeats < 0) {
+        return 'ERROR'
+    } else {
+
+        let i = repeats;
+        let concatinatedString ='';
+        
+        while (i !== 0) {
+            concatinatedString += string;
+            
+            i--;
+        }
+        //console.log(concatinatedString);
+        return concatinatedString;
+    }
 
 }
 
