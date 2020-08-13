58b7aa6abbc7e3ca7dd4527ac42f7141cbbf0124
Ricardo J Benavides <rickyjbena@yahoo.com>
2020-08-12 20:07:24 -0700

    Finished all of Part 1 odin projects

--- javascript-exercises/javascript-exercises-master/tempConversion/tempConversion.js
+++ javascript-exercises/javascript-exercises-master/tempConversion/tempConversion.js
@@ -1,9 +1,18 @@
-const ftoc = function() {
+const ftoc = function(temp) {
 
+   let tempC = (temp -32 ) * (5/9);
+   if (Number.isInteger(tempC)){
+     return tempC;
+   }
+   return +tempC.toFixed(1);
 }
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let tempF =(temp *(9 / 5) + 32)
+  if (Number.isInteger(tempF)){
+    return tempF;
+  }
+  return +tempF.toFixed(1);
 }
 
 module.exports = {
