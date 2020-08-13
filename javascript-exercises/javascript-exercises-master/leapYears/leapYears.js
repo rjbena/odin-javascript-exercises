e3c5b95fcdced3e62f2289809d4b1cea4f0dc018
Ricardo J Benavides <rickyjbena@yahoo.com>
2020-08-12 19:51:49 -0700

    finished leapYears

--- javascript-exercises/javascript-exercises-master/leapYears/leapYears.js
+++ javascript-exercises/javascript-exercises-master/leapYears/leapYears.js
@@ -1,5 +1,20 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  if (typeof year !== "number") {
+      return "ERROR";
+  }
+  if (year % 4 === 0 ) {
+      if (year % 100 === 0) {
+          if (year % 400 === 0) {
+              return true;
+          } else {
+              return false;
+          }
+      } else {
+          return true;
+      }
+  } else {
+      return false;
+  }
+};
 
-}
-
-module.exports = leapYears
+module.exports = leapYears;
