fc4dd35a0163efd24e74f1c1735166ae9cef8567
Ricardo J Benavides <rickyjbena@yahoo.com>
2020-08-25 10:54:21 -0700

    finished fibonacci

--- javascript-exercises/javascript-exercises-master/fibonacci/fibonacci.js
+++ javascript-exercises/javascript-exercises-master/fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(value) {
+value = parseInt(value);
+if (value < 0){
+    return "OOPS"
+}
+if (value === 1 || value ===2){
+    return 1;
+}
+let result;
+let prev = 1; 
+let secondPrev = 1;
 
+for (let i = 2; i < value; i++){
+    result = prev + secondPrev;
+    secondPrev = prev;
+    prev = result;
+}
+    return result;
 }
 
 module.exports = fibonacci
