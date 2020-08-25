b2d45cd3f18bb07f0481fa9f1bf721af34cd489b
Ricardo J Benavides <rickyjbena@yahoo.com>
2020-08-24 20:29:11 -0700

    finished palindrome

--- javascript-exercises/javascript-exercises-master/palindromes/palindromes.js
+++ javascript-exercises/javascript-exercises-master/palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function() {
+const palindromes = function(string) {
+    let punctuationless = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+     punctuationless= punctuationless.split(" ").join("");
+    punctuationless = punctuationless.toLowerCase();
 
+    console.log(punctuationless);
+    let back = punctuationless.length;
+    back--;
+    for (let i =0; i <back; i++){
+        console.log(punctuationless[i] + punctuationless[back])
+        if (punctuationless[i] !== punctuationless[back]){
+            return false;
+        }
+        back--;
+    }
+    return true;
 }
 
 module.exports = palindromes
