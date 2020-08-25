f30173a8d9105240c5b12b16159d23bd22086ce2
Ricardo J Benavides <rickyjbena@yahoo.com>
2020-08-24 21:25:05 -0700

    Did caesar cipher

--- javascript-exercises/javascript-exercises-master/caesar/caesar.js
+++ javascript-exercises/javascript-exercises-master/caesar/caesar.js
@@ -1,5 +1,23 @@
-const caesar = function() {
+const caesar = function (str, shift) {
+  if (shift < 0) {
+    return caesar(str, shift + 26);
+  }
 
-}
+  let result = "";
 
-module.exports = caesar
+  for (let i = 0; i < str.length; i++) {
+    let charcode = str[i];
+    if (charcode.match(/[a-z]/i)) {
+      let code = str.charCodeAt(i);
+      if (code >= 65 && code <= 90) {
+        charcode = String.fromCharCode(((code - 65 + shift) % 26) + 65);
+      } else if (code >= 97 && code <= 122) {
+        charcode = String.fromCharCode(((code - 97 + shift) % 26) + 97);
+      }
+    }
+    result += charcode;
+  }
+  return result;
+};
+
+module.exports = caesar;
